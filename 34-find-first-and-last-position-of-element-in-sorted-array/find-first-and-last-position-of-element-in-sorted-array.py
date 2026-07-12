29ef31d352427dee9b5b9513ee840f83e70b4a56
Mayank Arora <mayankarora25071@gmail.com>
2026-07-13 02:41:07 +0530

    Time: 3 ms (7.68%) | Memory: 20.5 MB (59.48%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,14 +1,51 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         n=len(nums)
-        first=-1
-        last=-1
-        for i in range(0,n):
-            if nums[i]==target:
-                if first==-1:
-                    first=i
-                last=i
-        return [first,last]
+        def lowerbound(nums,target):
+            low=0
+            high=n-1
+            lb=-1
+            while low<=high:
+                mid=(low+high)//2
+                if nums[mid]>=target:
+                    lb=mid
+                    high=mid-1
+                else:
+                    low=mid+1
+            return lb
+
+        def highbound(nums,target):
+            low=0
+            high=n-1
+            hb=-1
+            while low<=high:
+                mid=(low+high)//2
+                if nums[mid]<=target:
+                    hb=mid
+                    low=mid+1
+                else:
+                    high=mid-1
+            return hb
+
+        lb=lowerbound(nums,target)
+        if lb==-1 or nums[lb]!=target:
+            return [-1,-1]
+        hb=highbound(nums,target)
+        return [lb,hb]
+
+
+    
+
+        
+
+        # first=-1
+        # last=-1
+        # for i in range(0,n):
+        #     if nums[i]==target:
+        #         if first==-1:
+        #             first=i
+        #         last=i
+        # return [first,last]
 
 
         
