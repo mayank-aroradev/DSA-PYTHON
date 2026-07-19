53c4c09e04665713613ccb42886da9ce610d1a38
Mayank Arora <mayankarora25071@gmail.com>
2026-07-20 01:45:19 +0530

    Time: 5 ms (74.73%) | Memory: 19.8 MB (99.76%) - LeetSync

--- 2737-row-with-maximum-ones/row-with-maximum-ones.py
+++ 2737-row-with-maximum-ones/row-with-maximum-ones.py
@@ -1,29 +1,42 @@
 class Solution:
-    def lowerbound(self,arr,n,x):
-        low=0
-        high=n-1
-        ans=n
-        while low<=high:
-            mid=(low+high)//2
-            if arr[mid]>=x:
-                ans=mid
-                high=mid-1
-            else:
-                low=mid+1
-        return ans
+    # def lowerbound(self,arr,n,x):
+    #     low=0
+    #     high=n-1
+    #     ans=n
+    #     while low<=high:
+    #         mid=(low+high)//2
+    #         if arr[mid]>=x:
+    #             ans=mid
+    #             high=mid-1
+    #         else:
+    #             low=mid+1
+    #     return ans
 
-    def rowAndMaximumOnes(self, mat: List[List[int]]) -> List[int]:
-        cnt_max = -1
-        index = 0
-        m = len(mat)
-        n = len(mat[0])
+    # def rowAndMaximumOnes(self, mat: List[List[int]]) -> List[int]:
+        # cnt_max = -1
+        # index = 0
+        # m = len(mat)
+        # n = len(mat[0])
 
-        for i in range(m):
-            sorted_row = sorted(mat[i])  
-            cnt_ones = n - self.lowerbound(sorted_row, n, 1)
-            if cnt_ones > cnt_max:
-                cnt_max = cnt_ones
-                index = i
+        # for i in range(m):
+        #     sorted_row = sorted(mat[i])  
+        #     cnt_ones = n - self.lowerbound(sorted_row, n, 1)
+        #     if cnt_ones > cnt_max:
+        #         cnt_max = cnt_ones
+        #         index = i
 
-        return [index, cnt_max]
-        
+        # return [index, cnt_max]
+
+        def rowAndMaximumOnes(self, mat: List[List[int]]) -> List[int]:
+            row_index = 0 
+            max_ones = 0
+
+            for i in range(len(mat)):
+                ones = sum(mat[i])
+
+                if ones > max_ones:
+                    max_ones = ones
+                    row_index = i
+
+            return [row_index,max_ones]  
+            
