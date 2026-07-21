47fce4679496befa8231aeeb0d9ecfad0f991f0b
Mayank Arora <mayankarora25071@gmail.com>
2026-07-22 02:28:52 +0530

    Time: 152 ms (11.39%) | Memory: 25.6 MB (59.54%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
@@ -2,30 +2,42 @@ class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         m=len(matrix)
         n=len(matrix[0])
-        if not matrix or not matrix:
-            return False
-        for i in range(m):
-            index=self.bs(matrix,i,target)
-            if index!=-1:
-                return True
+    #     if not matrix or not matrix:
+    #         return False
+    #     for i in range(m):
+    #         index=self.bs(matrix,i,target)
+    #         if index!=-1:
+    #             return True
             
-        return False
+    #     return False
 
 
-    def bs(self,matrix,i,target):
-        n=len(matrix[0])
-        low=0
-        high=n-1
-        while low<=high:
-            mid=(low+high)//2
-            if matrix[i][mid]==target:
-                return mid
-            elif matrix[i][mid]<target:
-                low=mid+1
+    # def bs(self,matrix,i,target):
+    #     n=len(matrix[0])
+    #     low=0
+    #     high=n-1
+    #     while low<=high:
+    #         mid=(low+high)//2
+    #         if matrix[i][mid]==target:
+    #             return mid
+    #         elif matrix[i][mid]<target:
+    #             low=mid+1
+    #         else:
+    #             high=mid-1
+    #     return -1
+
+#  optimal sol
+
+        row=0
+        col=n-1
+        while row<m and col>=0:
+            if matrix[row][col]==target:
+                return True
+            elif matrix[row][col]<target:
+                row+=1
             else:
-                high=mid-1
-        return -1
-
+                col-=1
+        return False
 
 
         
