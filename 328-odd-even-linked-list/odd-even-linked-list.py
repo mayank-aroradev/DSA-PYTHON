0ed9fb3b23a34130acf4df31f737e278b277b2cb
Mayank Arora <mayankarora25071@gmail.com>
2026-07-28 03:57:33 +0530

    Time: 0 ms (100.00%) | Memory: 20.9 MB (99.30%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.py
+++ 328-odd-even-linked-list/odd-even-linked-list.py
@@ -5,29 +5,50 @@
 #         self.next = next
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        values=[]
-        temp=head
-        if not head or not head.next:
-            return head
+
+        # brute force
+        # values=[]
+        # temp=head
+        # if not head or not head.next:
+        #     return head
             
-        values = []
-        temp = head
-        while temp:
-            values.append(temp.val)
-            temp = temp.next.next if temp.next else None
+        # values = []
+        # temp = head
+        # while temp:
+        #     values.append(temp.val)
+        #     temp = temp.next.next if temp.next else None
             
-        temp = head.next
-        while temp:
-            values.append(temp.val)
-            temp = temp.next.next if temp.next else None
+        # temp = head.next
+        # while temp:
+        #     values.append(temp.val)
+        #     temp = temp.next.next if temp.next else None
 
 
 
-        temp=head
-        index=0
-        while temp is not None:
-            temp.val=values[index]
-            index+=1
-            temp=temp.next
-        return head
+        # temp=head
+        # index=0
+        # while temp is not None:
+        #     temp.val=values[index]
+        #     index+=1
+        #     temp=temp.next
+        # return head
         
+        # optimal force
+
+        if not head or not head.next:
+
+                return head
+
+        odd=head
+        even=head.next
+        even_head=even
+        while even is not None and even.next is not None:
+            odd.next=odd.next.next
+            odd=odd.next
+            even.next=even.next.next
+            even=even.next
+        
+        odd.next=even_head
+        return head
+
+
