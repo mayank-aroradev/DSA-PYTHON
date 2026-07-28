c1f140110f2ed67c7bd4ef437664e71ce60b48dc
Mayank Arora <mayankarora25071@gmail.com>
2026-07-29 01:01:06 +0530

    Time: 276 ms (49.77%) | Memory: 21.9 MB (9.68%) - LeetSync

---
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
@@ -0,0 +1,8 @@
+class Solution:
+    def smallestPalindrome(self, s: str) -> str:
+        partition=len(s)//2
+        base=sorted(s[:partition])
+        mid=[s[partition]] if len(s) % 2 ==1 else[]
+        reversed_base=base[::-1]
+
+        return "".join(base+mid+reversed_base)
