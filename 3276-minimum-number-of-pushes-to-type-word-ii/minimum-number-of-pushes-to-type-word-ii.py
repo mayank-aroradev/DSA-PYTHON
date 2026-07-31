67fec01f337266379f1459dda9a32cecbe744724
Mayank Arora <mayankarora25071@gmail.com>
2026-07-31 21:31:18 +0530

    Time: 187 ms (25.42%) | Memory: 20 MB (48.16%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        frequency=[0]*26
+        for c in word:
+            frequency[ord(c)-ord("a")]+=1
+
+        frequency.sort(reverse=True)
+
+
+        total_push=0
+        for i in range(26):
+            if frequency[i]==0:
+                break
+            total_push+=(i//8+1)*frequency[i]
+        return total_push
+
+
+
+        
