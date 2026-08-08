ca6da2a77829e8d642279b16e4c38f0bffecfc43
Mayank Arora <mayankarora25071@gmail.com>
2026-08-09 00:05:32 +0530

    Time: 3 ms (70.34%) | Memory: 20.6 MB (45.24%) - LeetSync

--- 137-single-number-ii/single-number-ii.py
+++ 137-single-number-ii/single-number-ii.py
@@ -1,10 +1,17 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        hash_map={}
-        for i in nums:
-            hash_map[i]=hash_map.get(i,0)+1
+        # hash_map={}
+        # for i in nums:
+        #     hash_map[i]=hash_map.get(i,0)+1
             
-        for key in hash_map:
-            if hash_map[key]==1:
-                return key
+        # for key in hash_map:
+        #     if hash_map[key]==1:
+        #         return key
         
+
+        nums.sort()
+        n=len(nums)
+        for i in range(1,n,3):
+            if nums[i]!=nums[i-1]:
+                return nums[i-1]
+        return nums[n-1]
