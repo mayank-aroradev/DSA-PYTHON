af605d0331bc15f70cc9af2952ff4aa59edfdd8d
Mayank Arora <mayankarora25071@gmail.com>
2026-09-05 01:51:40 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (88.57%) - LeetSync

---
+++ 342-power-of-four/power-of-four.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n<=0:
+            return False
+        if n==1:
+            return True
+        
+        while n%4==0:
+            n=n//4
+        return n==1
+
+        
