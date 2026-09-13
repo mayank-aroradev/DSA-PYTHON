ecb53d8ccb999d9af6d0ad4ccbebb4354f671051
Mayank Arora <mayankarora25071@gmail.com>
2026-09-14 03:10:38 +0530

    Time: 3 ms (33.82%) | Memory: 19.3 MB (62.56%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,15 +1,24 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        def check(n):
-            if n==1:
-                return True
-            if n==4:
-                return False
-            sum_d=sum(int(digit)**2 for digit in str(n))
+        # def check(n):
+        #     if n==1:
+        #         return True
+        #     if n==4:
+        #         return False
+        #     sum_d=sum(int(digit)**2 for digit in str(n))
 
-            return check(sum_d)
+        #     return check(sum_d)
         
-        return check(n)
+        # return check(n)
+        # O(logn)
+        # O(log(n))
+        seen=set()
+
+        while n!=1 and n not in seen:
+            seen.add(n)
+            n=sum(int(digit)**2 for digit in str(n))
+            
+        return n==1
 
         
 
