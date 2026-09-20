7545ba15d9d01d7d7d04fabe2d1713efcdf8c18b
Mayank Arora <mayankarora25071@gmail.com>
2026-09-20 23:48:06 +0530

    Time: 4 ms (58.16%) | Memory: 19.5 MB (93.54%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -1,21 +1,49 @@
 class Solution:
     def permuteUnique(self, nums: list[int]) -> list[list[int]]:
+    #     ans=[]
+    #     def permutations(index,nums,ans):
+    #         if index==len(nums):
+    #             ds=[]
+    #             for i in range(len(nums)):
+    #                 ds.append(nums[i])
+    #             if ds not in ans:
+    #                 ans.append(ds)
+    #             return 
+    #         for i in range (index,len(nums)):
+    #             swap(i,index,nums)
+    #             permutations(index+1,nums,ans)
+    #             swap(i,index,nums)
+        
+    #     def swap(i,j,nums):
+    #         nums[i],nums[j]=nums[j],nums[i]
+        
+    #     permutations(0,nums,ans)
+    #     return ans
+
+    # tc-O(n!*n^2)
+    # sc-O(n!*n)
+
         ans=[]
-        def permutations(index,nums,ans):
-            if index==len(nums):
-                ds=[]
-                for i in range(len(nums)):
-                    ds.append(nums[i])
-                if ds not in ans:
-                    ans.append(ds)
+        ds=[]
+        freq=[False]*len(nums)
+        nums.sort()
+        
+        
+        def recruteperm(nums,ds,ans,freq):
+            if len(ds)==len(nums):
+                ans.append(ds[:])
                 return 
-            for i in range (index,len(nums)):
-                swap(i,index,nums)
-                permutations(index+1,nums,ans)
-                swap(i,index,nums)
+
+            for i in range(len(nums)):
+                if  freq[i]:
+                    continue
+                if i>0 and nums[i]==nums[i-1] and not freq[i-1]:
+                    continue
+                freq[i]=True
+                ds.append(nums[i])
+                recruteperm(nums,ds,ans,freq)
+                ds.pop()
+                freq[i]=False
         
-        def swap(i,j,nums):
-            nums[i],nums[j]=nums[j],nums[i]
-        
-        permutations(0,nums,ans)
+        recruteperm(nums,ds,ans,freq)
         return ans
